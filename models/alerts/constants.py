729346dd432783b160cd94faee6d70f02a953013
Kevin Bardool <Kbardool@aol.com>
2016-10-19 01:06:45 -0700

    Added environment variables and UWSGI

--- models/alerts/constants.py
+++ models/alerts/constants.py
@@ -1,7 +1,13 @@
+import os
+
 __author__ = 'KBardool'
 
-URL = "https://api.mailgun.net/v3/sandbox0494fd345970485dbcc98b1aa757bd19.mailgun.org/messages"
-APIKEY = "key-f7dd3239713abc6c481f005d12290bff"
-FROM = "postmaster@sandbox0494fd345970485dbcc98b1aa757bd19.mailgun.org"
-TO = "Kevin Bardool <kbardool@outlook.com>"
+URL = os.environ.get(MAILGUN_URL)
+    # "https://api.mailgun.net/v3/sandbox0494fd345970485dbcc98b1aa757bd19.mailgun.org/messages"
+APIKEY = os.environ.get(MAILGUN_API_KEY)
+    # "key-f7dd3239713abc6c481f005d12290bff"
+FROM = os.environ.get(MAILGUN_FROM)
+    # "postmaster@sandbox0494fd345970485dbcc98b1aa757bd19.mailgun.org"
+TO = os.environ.get(MAILGUN_TO)
+    # "Kevin Bardool <kbardool@outlook.com>"
 SUBJECT = "Hello Kevin Bardool"
