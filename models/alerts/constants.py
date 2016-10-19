04544026f55b73458648f1e519f7ec8d73a633bc
Kevin Bardool <Kbardool@aol.com>
2016-10-19 03:00:02 -0700

    Fixed app.config.from_object('config') to ('src_config')

--- models/alerts/constants.py
+++ models/alerts/constants.py
@@ -2,12 +2,12 @@ import os
 
 __author__ = 'KBardool'
 
-URL = os.environ.get('https://api.mailgun.net/v3/sandbox0494fd345970485dbcc98b1aa757bd19.mailgun.org/messages')
+URL = os.environ.('MAILGUN_URL')
     # "https://api.mailgun.net/v3/sandbox0494fd345970485dbcc98b1aa757bd19.mailgun.org/messages"
-APIKEY = os.environ.get(MAILGUN_API_KEY)
+APIKEY = os.environ.get('MAILGUN_API_KEY')
     # "key-f7dd3239713abc6c481f005d12290bff"
-FROM = os.environ.get(MAILGUN_FROM)
+FROM = os.environ.get('MAILGUN_FROM')
     # "postmaster@sandbox0494fd345970485dbcc98b1aa757bd19.mailgun.org"
-TO = os.environ.get(MAILGUN_TO)
+TO = os.environ.get('MAILGUN_TO')
     # "Kevin Bardool <kbardool@outlook.com>"
 SUBJECT = "Hello Kevin Bardool"
